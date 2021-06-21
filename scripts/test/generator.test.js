f106d863e337a5b3cf868009aac6c1abdf4b5825
tamgmouren <83946821+zixingtangmouren@users.noreply.github.com>
2021-06-21 23:22:28 +0800

    增加package合并方法

--- scripts/test/generator.test.js
+++ scripts/test/generator.test.js
@@ -2,13 +2,15 @@
  * @Author: tangzhicheng
  * @Date: 2021-06-20 22:54:22
  * @LastEditors: tangzhicheng
- * @LastEditTime: 2021-06-21 22:31:45
+ * @LastEditTime: 2021-06-21 23:21:31
  * @Description: file content
  */
 
 const { describe, it } = require('mocha')
 const path = require('path')
+const assert = require('assert').strict
 const { checkExit, rm } = require('../../lib/utils/fileActions')
+const Generator = require('../../lib/Generator')
 
 describe('check generator', () => {
   it('check babel', async () => {
@@ -22,5 +24,39 @@ describe('check generator', () => {
   it('check src', async () => {
     const srcGenerator = require('../../lib/generator/src')
     await srcGenerator({ Selected: { framework: 'react' }, package: { name: 'app-demo' } })
+  })
+
+  it('check Generator extendPackage function', () => {
+    const generator = new Generator()
+
+    generator.extendPackage({
+      devDependencies: {
+        '@babel/core': '^7.14.2',
+        '@babel/polyfill': '^7.12.1',
+        'babel-eslint': '^10.1.0',
+        'babel-loader': '^8.2.2',
+      },
+    })
+
+    generator.extendPackage({
+      devDependencies: {
+        '@babel/preset-env': '^7.14.2',
+        '@babel/preset-react': '^7.13.13',
+      },
+    })
+
+    assert.deepEqual(generator.package, {
+      name: '',
+      version: '1.0.0',
+      description: '',
+      devDependencies: {
+        '@babel/core': '^7.14.2',
+        '@babel/polyfill': '^7.12.1',
+        'babel-eslint': '^10.1.0',
+        'babel-loader': '^8.2.2',
+        '@babel/preset-env': '^7.14.2',
+        '@babel/preset-react': '^7.13.13',
+      },
+    })
   })
 })
