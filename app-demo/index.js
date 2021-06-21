eca8fe0e18b2a15844a742ca8ceebf51d1d43f67
tamgmouren <83946821+zixingtangmouren@users.noreply.github.com>
2021-06-21 23:26:53 +0800

    fix

--- app-demo/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * @Author: tangzhicheng
- * @Date: 2021-06-21 14:40:53
- * @LastEditors: tangzhicheng
- * @LastEditTime: 2021-06-21 14:46:56
- * @Description: file content
- */
-
-import './App'
