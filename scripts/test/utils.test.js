7d244fb4c75f3379e2c7eaf1d0ec41955860d9ab
tamgmouren <83946821+zixingtangmouren@users.noreply.github.com>
2021-06-21 17:12:42 +0800

    fix

--- scripts/test/utils.test.js
+++ scripts/test/utils.test.js
@@ -2,7 +2,7 @@
  * @Author: tangzhicheng
  * @Date: 2021-06-20 11:27:31
  * @LastEditors: tangzhicheng
- * @LastEditTime: 2021-06-21 16:45:03
+ * @LastEditTime: 2021-06-21 17:12:34
  * @Description: 测试工具方法
  */
 
@@ -13,28 +13,28 @@ const {
 } = require('../../lib/utils/fileActions')
 
 describe('check utils function', () => {
-  // it('check checkExit function', (done) => {
-  //   checkExit(path.join(process.cwd(), 'bin')).then((isExit) => {
-  //     if (isExit) {
-  //       done()
-  //     } else {
-  //       done(new Error('方法异常！'))
-  //     }
-  //   }).catch((err) => done(err))
-  // })
+  it('check checkExit function', (done) => {
+    checkExit(path.join(process.cwd(), 'bin')).then((isExit) => {
+      if (isExit) {
+        done()
+      } else {
+        done(new Error('方法异常！'))
+      }
+    }).catch((err) => done(err))
+  })
 
-  // it('check createDir function', (done) => {
-  //   const dirCode = parseInt((Math.random() * (10 ** 6)).toFixed(6))
-  //   const dirName = `testDir${dirCode}`
-  //   const dirPath = path.join(process.cwd(), dirName)
-  //   createDir(dirName)
-  //     .then(() => checkExit(dirPath))
-  //     .then(() => rmdir(dirPath))
-  //     .then(() => done())
-  //     .catch((err) => done(err))
-  // })
+  it('check createDir function', async () => {
+    const dirCode = parseInt((Math.random() * (10 ** 6)).toFixed(6))
+    const dirName = `testDir${dirCode}`
+    const dirPath = path.join(process.cwd(), dirName)
+    await createDir(dirName)
+    await checkExit(dirPath)
+    await rmdir(dirPath)
+  })
 
   it('check copyDir', async () => {
-    await copyDir(path.join(process.cwd(), 'scripts'), path.join(process.cwd(), 'copy'))
+    const source = path.join(process.cwd(), 'scripts')
+    const target = path.join(process.cwd(), 'copy')
+    await copyDir(source, target)
   })
 })
