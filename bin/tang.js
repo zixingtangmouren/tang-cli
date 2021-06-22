f02b19ebb435d82d73e58bd8006f339fcf16a454
tamgmouren <83946821+zixingtangmouren@users.noreply.github.com>
2021-06-22 19:14:46 +0800

    正式发布v2版本

--- bin/tang.js
+++ bin/tang.js
@@ -3,7 +3,7 @@ const program = require('commander')
 const create = require('../lib/create')
 
 program
-  .version('1.1.0')
+  .version('2.0.1')
   .command('create <name>')
   .description('create a new project')
   .action((name) => {
