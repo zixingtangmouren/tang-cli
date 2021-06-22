3719e54b9ab0cca7ac4c6b83d3b41208764394f0
tamgmouren <83946821+zixingtangmouren@users.noreply.github.com>
2021-06-22 23:14:46 +0800

    fix

--- bin/tang.js
+++ bin/tang.js
@@ -3,7 +3,7 @@ const program = require('commander')
 const create = require('../lib/create')
 
 program
-  .version('2.0.1')
+  .version('2.0.2')
   .command('create <name>')
   .description('create a new project')
   .action((name) => {
